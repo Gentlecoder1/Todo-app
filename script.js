243730cc550be95f56310d2c531dae3dddc3a3b0
Israel <israeloloruntoba3@email.com>
2025-09-25 14:35:04 +0100

    Enhance todo functionality by adding task creation and deletion features

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // const itemsLeft = document.getElementById("items-left");
   // const clearCompleted = document.getElementById("clear-completed");
   // const todoList = document.getElementById("todo-list");
   // const filterAll = document.getElementById("filter-all");
   // const filterActive = document.getElementById("filter-active");
   // const filterCompleted = document.getElementById("filter-completed");
-  const crossIcon = document.getElementById("cross-icon");
 
   const taskForm = document.getElementById("form");
   const field = document.getElementById("field");
@@ -55,15 +53,86 @@ document.addEventListener("DOMContentLoaded", () => {
   moonIcon.addEventListener("click", () => toggleTheme(true));
   sunIcon.addEventListener("click", () => toggleTheme(false));
 
-  const taskList = document.querySelectorAll(".tasks");
-  taskList.forEach((task) => {
-    const checkIcon = task.querySelector(".check-icon");
-    const checkTask = task.querySelector(".checkTask"); 
-    const todo = task.querySelector("#task-text"); 
+  
+
+  const taskInput = document.getElementById("task-input");
+  const todoTasks = document.getElementById("todo-tasks");
+  const itemsLeft = document.getElementById("items-left");
+  let todonum = 0;
+
+  function updateItemsLeft() {
+    itemsLeft.innerHTML = `${todonum} items left`
+  }
+
+  //function to addtask
+  function addTask() {
+    const itemValue = taskInput.value.trim();
+    if (itemValue === "") return;
+
+
+    const newTodo = document.createElement("li");
+    newTodo.className = "tasks group cursor-pointer"
+
+    newTodo.innerHTML = `
+      <div class="flex justify-between items-center px-6 py-4">
+        <span class="updated-task flex space-x-2">
+          <button id="btn" class="checkTask items-center flex cursor-pointer border-2 border-gray-200 group-hover:border-gray-400 rounded-full w-6 h-6">
+            <img
+              src="./images/icon-check.svg"
+              alt="check icon"
+              id=""
+              class="check-icon w-full h-full hidden rounded-full p-1 bg-gradient-to-br from-blue-400 to-purple-800"
+            />
+          </button>            
+          <p class="text-left text-[13px]"  id="task-text">${itemValue}</p>
+        </span>
+
+        <span class="hidden group-hover:block"><img
+          src="./images/icon-cross.svg"
+          alt="cross icon"
+          id="cross-icon"
+          class="w-4 h-4 ml-4"
+          />
+        </span>
+      </div>
+      <hr class="border-t border-gray-500" />`
+    // add newtask in the ul
+    todoTasks.appendChild(newTodo);
+    // reset taskform to empty
+    taskInput.value = "";
+      
+
+    // toggle functionality for the newtodo
+    const checkIcon = newTodo.querySelector(".check-icon");
+    const checkTask = newTodo.querySelector(".checkTask"); 
+    const todo = newTodo.querySelector("#task-text"); 
 
     checkTask.addEventListener("click", () => {
       checkIcon.classList.toggle("hidden");
       todo.classList.toggle("line-through");
     })
+
+    // To remove a todo
+    const cancel = newTodo.querySelector("#cross-icon"); 
+    cancel.addEventListener("click", () => {
+      console.log("task-deleted")
+      newTodo.remove();
+
+      // decrement items left
+      todonum--;
+      updateItemsLeft();
+    })
+    
+    // increment items left
+    todonum++;
+    updateItemsLeft();
+  }
+    
+  // addtask on enter
+  taskInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      console.log("task-added")
+      addTask();
+    }
   })
 });
